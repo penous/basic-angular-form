d514eb4fa91b628ddb6545f971a26b6858e3ced3
Tom Dirinck <tom.dirinck@gmail.com>
2021-07-14 15:21:50 +0200

    Added server.

--- server/server.js
+++ server/server.js
@@ -2,31 +2,51 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
-const PORT = 6969;
+const PORT = 9000;
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}) );
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.all("/*", function(req, res, next){
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-    next();
+app.all('/*', function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Content-Type, Authorization, Content-Length, X-Requested-With'
+  );
+  next();
 });
 
-
-let allFriends = [{fName: 'Coach', lName: 'Tim', email: 'tim.broos@becode.org', phone: '0469420666', signatureMove: 'Yeet', language: 'Javascript'}];
+let allFriends = [
+  {
+    firstName: 'Coach',
+    lastName: 'Tim',
+    email: 'tim.broos@becode.org',
+    phoneNumber: '0469420666',
+    language: 'Js',
+  },
+];
 
 // Below you can define how your API handles a get or a post request.
 // Try sending a get request to the root, you should get a "Hello from server" back.
 
 app.get('/', function (request, response) {
-    response.send('Hello from server');
+  response.send('Hello from server');
+});
+
+app.get('/allFriends', (request, response) => {
+  response.send(allFriends);
 });
 
 app.post('/', function (request, response) {
-    response.status(200).send({"message": "Data received"});
+  response.status(200).send({ message: 'Data received' });
 });
 
+app.post('/addFriend', (request, response) => {
+  allFriends[allFriends.length] = request.body;
+  response.status(200).send({ message: 'Friend added' });
+});
 
-app.listen(PORT, function () {});
+app.listen(PORT, function () {
+  console.log(`Running on port ${PORT}...`);
+});
