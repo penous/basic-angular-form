5fd827a3570091518061e8463471811fff1156cf
Tom Dirinck <tom.dirinck@gmail.com>
2021-08-07 14:26:05 +0200

    Implemented mongoose and created GET and POST for friends.

--- server/server.js
+++ server/server.js
@@ -1,8 +1,22 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const app = express();
 
 const PORT = 9000;
+
+mongoose.connect(
+  'mongodb+srv://penous:penous@becode.v7mmb.mongodb.net/friendBook',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+const db = mongoose.connection;
+db.once('open', () => {
+  console.log('Connected to MongoDB cluster');
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -16,6 +30,18 @@ app.all('/*', function (req, res, next) {
   );
   next();
 });
+
+// * Create schema for mongoose models
+const friendSchema = new mongoose.Schema({
+  firstName: 'string',
+  lastName: 'string',
+  email: 'string',
+  phoneNumber: 'string',
+  language: 'string',
+});
+
+// * Create mongoose models
+const Friend = mongoose.model('Friend', friendSchema, 'friends');
 
 let allFriends = [
   {
@@ -35,7 +61,11 @@ app.get('/', function (request, response) {
 });
 
 app.get('/allFriends', (request, response) => {
-  response.send(allFriends);
+  Friend.find((err, friends) => {
+    if (err) console.log(err);
+    console.log(friends);
+    response.send(friends);
+  });
 });
 
 app.post('/', function (request, response) {
@@ -43,6 +73,19 @@ app.post('/', function (request, response) {
 });
 
 app.post('/addFriend', (request, response) => {
+  // Create friend and add to mongodb cluster
+  const friend = new Friend({
+    firstName: request.body.firstName,
+    lastName: request.body.lastName,
+    email: request.body.email,
+    phoneNumber: request.body.phoneNumber,
+    language: request.body.language,
+  });
+  friend.save((err, friend) => {
+    if (err) console.log(err);
+    console.log(friend);
+  });
+
   allFriends[allFriends.length] = request.body;
   response.status(200).send({ message: 'Friend added' });
 });
